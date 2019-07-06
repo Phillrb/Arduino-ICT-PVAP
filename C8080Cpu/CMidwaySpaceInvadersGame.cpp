9d3aa877e2639e22b84a879bd240b62d9d47934e
Phillip Riscombe-Burton <phillrb@gmail.com>
2019-07-06 08:07:18 +0100

    Update CMidwaySpaceInvadersGame.cpp

--- C8080Cpu/CMidwaySpaceInvadersGame.cpp
+++ C8080Cpu/CMidwaySpaceInvadersGame.cpp
@@ -71,9 +71,9 @@ static const ROM_REGION s_romRegionSV2[] PROGMEM = { //
                                                         {NO_BANK_SWITCH, 0x0000, 0x0400, s_romData2nSV2Rev2_r36, 0xd0c32d72, "r36"}, //
                                                         {NO_BANK_SWITCH, 0x0400, 0x0400, s_romData2nSV2Rev2_r35, 0x0e159534, "r35"}, //
                                                         {NO_BANK_SWITCH, 0x0800, 0x0400, s_romData2nSV2Rev2_r34, 0x483e651e, "r34"}, //
-                                                        {NO_BANK_SWITCH, 0x1400, 0x0400, s_romData2nSV2Rev2_r31, 0x1293b826, "r31"}, //
-                                                        {NO_BANK_SWITCH, 0x1800, 0x0400, s_romData2nSV2Rev2_r42, 0xcd80b13f, "r42"}, //
-                                                        {NO_BANK_SWITCH, 0x1C00, 0x0400, s_romData2nSV2Rev2_r41, 0x2c68e0b4, "r41"}, //
+                                                        {NO_BANK_SWITCH, 0x0C00, 0x0400, s_romData2nSV2Rev2_r31, 0x1293b826, "r31"}, //
+                                                        {NO_BANK_SWITCH, 0x1000, 0x0400, s_romData2nSV2Rev2_r42, 0xcd80b13f, "r42"}, //
+                                                        {NO_BANK_SWITCH, 0x1400, 0x0400, s_romData2nSV2Rev2_r41, 0x2c68e0b4, "r41"}, //
                                                         {0} }; // end of list
 
 //
@@ -83,9 +83,9 @@ static const ROM_REGION s_romRegionSU[] PROGMEM = { //
                                                         {NO_BANK_SWITCH, 0x0000, 0x0400, s_romData2nSU_1, 0x7a9b4485, "r36"}, //
                                                         {NO_BANK_SWITCH, 0x0400, 0x0400, s_romData2nSU_2, 0x7c86620d, "r35"}, //
                                                         {NO_BANK_SWITCH, 0x0800, 0x0400, s_romData2nSU_3, 0xccaf38f6, "r34"}, //
-                                                        {NO_BANK_SWITCH, 0x1400, 0x0400, s_romData2nSU_4, 0x8ec9eae2, "r31"}, //
-                                                        {NO_BANK_SWITCH, 0x1800, 0x0400, s_romData2nSU_5, 0x68719b30, "r42"}, //
-                                                        {NO_BANK_SWITCH, 0x1C00, 0x0400, s_romData2nSU_6, 0x8abe2466, "r41"}, //
+                                                        {NO_BANK_SWITCH, 0x0C00, 0x0400, s_romData2nSU_4, 0x8ec9eae2, "r31"}, //
+                                                        {NO_BANK_SWITCH, 0x1000, 0x0400, s_romData2nSU_5, 0x68719b30, "r42"}, //
+                                                        {NO_BANK_SWITCH, 0x1400, 0x0400, s_romData2nSU_6, 0x8abe2466, "r41"}, //
                                                         {0} }; // end of list
 
 //
@@ -95,11 +95,11 @@ static const ROM_REGION s_romRegionSURD[] PROGMEM = { //
                                                         {NO_BANK_SWITCH, 0x0000, 0x0400, s_romData2nSURD_1, 0xF625F153, "1 H"}, //
                                                         {NO_BANK_SWITCH, 0x0400, 0x0400, s_romData2nSURD_2, 0x9D628753, "2 G"}, //
                                                         {NO_BANK_SWITCH, 0x0800, 0x0400, s_romData2nSURD_3, 0xDB342868, "3 F"}, //
-                                                        {NO_BANK_SWITCH, 0x1400, 0x0400, s_romData2nSURD_4, 0xB635E8AC, "4 E"}, //
-                                                        {NO_BANK_SWITCH, 0x1800, 0x0400, s_romData2nSURD_5, 0x014E761F, "5 D"}, //
-                                                        {NO_BANK_SWITCH, 0x1C00, 0x0400, s_romData2nSURD_6, 0x599C6B78, "6 C"}, //
-                                                        {NO_BANK_SWITCH, 0x2000, 0x0400, s_romData2nSURD_7, 0x0E8A75DF, "7 B"}, //
-                                                        {NO_BANK_SWITCH, 0x2400, 0x0400, s_romData2nSURD_8, 0x11392151, "8 A"}, //
+                                                        {NO_BANK_SWITCH, 0x0C00, 0x0400, s_romData2nSURD_4, 0xB635E8AC, "4 E"}, //
+                                                        {NO_BANK_SWITCH, 0x1000, 0x0400, s_romData2nSURD_5, 0x014E761F, "5 D"}, //
+                                                        {NO_BANK_SWITCH, 0x1400, 0x0400, s_romData2nSURD_6, 0x599C6B78, "6 C"}, //
+                                                        {NO_BANK_SWITCH, 0x1800, 0x0400, s_romData2nSURD_7, 0x0E8A75DF, "7 B"}, //
+                                                        {NO_BANK_SWITCH, 0x1C00, 0x0400, s_romData2nSURD_8, 0x11392151, "8 A"}, //
                                                         {0} }; // end of list
 
 
